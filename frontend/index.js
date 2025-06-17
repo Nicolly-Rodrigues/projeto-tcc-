dc0a84f806b2e9693fe8aeb772a34824281ab902
Nicolly-Rodrigues <nicollyrodrigues.dev2024@gmail.com>
2025-06-17 11:17:32 -0300

    acabou amem

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,11 @@
-function cadastrar() {
-    const login = document.getElementById("login").value;
-    const senha = document.getElementById("senha").value;
+function irParaHome() {
+  const login = document.getElementById("login").value;
+  const senha = document.getElementById("senha").value;
 
-    if (login && senha) {
-
-      window.location.href = "home.html";
-    } else {
-      alert("Por favor, preencha todos os campos.");
-    }
+  if (login && senha) {
+    // Simula cadastro e redireciona
+    window.location.href = "home.html";
+  } else {
+    alert("Preencha todos os campos!");
   }
+}
